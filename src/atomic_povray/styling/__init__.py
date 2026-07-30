ffc2e2ba0815009c7719e68bdbd69615c0bae0fa
Florian Kraushofer <66359721+fkraushofer@users.noreply.github.com>
2026-07-30 16:59:05 +0200

    Export atom style rules

--- src/atomic_povray/styling/__init__.py
+++ src/atomic_povray/styling/__init__.py
@@ -1,17 +1,25 @@
 from .styles import (
+    AtomSelectionRule,
     AtomStyle,
+    AtomStyleOverride,
     BondStyle,
+    CoordinationStyleRule,
     DepthShading,
     StyleConfig,
     StyledGeometry,
     apply_styles,
+    resolve_atom_styles,
 )
 
 __all__ = [
+    "AtomSelectionRule",
     "AtomStyle",
+    "AtomStyleOverride",
     "BondStyle",
+    "CoordinationStyleRule",
     "DepthShading",
     "StyleConfig",
     "StyledGeometry",
     "apply_styles",
+    "resolve_atom_styles",
 ]
