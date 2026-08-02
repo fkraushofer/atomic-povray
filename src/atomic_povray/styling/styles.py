5b59c358f40da523a7e3704c730675fc47d408a0
Florian Kraushofer <66359721+fkraushofer@users.noreply.github.com>
2026-08-02 18:50:30 +0200

    Keep alpha alias compatible with transparency default

--- src/atomic_povray/styling/styles.py
+++ src/atomic_povray/styling/styles.py
@@ -289,8 +289,8 @@ class PolyhedronStyle:
     color: Color | None = None
     material: Material | None = None
     finish: Finish | None = None
-    filter: float | None = 0.05
-    transmit: float | None = 0.3
+    filter: float | None = None
+    transmit: float | None = None
     alpha: float | None = None
     edges: PolyhedronEdgeStyle = PolyhedronEdgeStyle()
 
@@ -405,7 +405,10 @@ class StyleConfig:
     )
     default_atom: AtomStyle = AtomStyle()
     default_bond: BondStyle = BondStyle()
-    default_polyhedron: PolyhedronStyle = PolyhedronStyle()
+    default_polyhedron: PolyhedronStyle = PolyhedronStyle(
+        filter=0.05,
+        transmit=0.3,
+    )
     default_atom_finish: Finish = Finish(phong=0.3)
     default_bond_finish: Finish = Finish()
     default_polyhedron_finish: Finish = Finish(phong=0.15)
